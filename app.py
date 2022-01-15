fad88e074e5d05c79eac2969e67f2ebd1eb26fa4
gkaczm <gabriela.kaczmarek99@gmail.com>
2022-01-15 21:23:52 +0100

    Style and view fixes

--- app.py
+++ app.py
@@ -26,13 +26,25 @@ def mainpage():  # put application's code here
     else:
         return render_template("index.html")
 
+
 @app.route('/mail', methods=["GET", "POST"])
 def classfmail():
     if request.method == "POST":
         index = int(request.form.get("index"))
         try:
             email = get_emails(index)
-        except:
+            list = email["from"].split()
+            print("c")
+            address = list[-1]
+            print("c")
+            address = address[:4] + (len(address)-13)*"*" + address[-13:]
+            print("c")
+            list[-1] = address
+            print("c")
+            email["from"] = " ".join(list)
+            print("c")
+        except Exception as e:
+            print(e)
             suggestion = "Please make sure there is enough emails"
             return render_template("apology.html", suggestion=suggestion)
         # TODO fetch email dict
@@ -49,7 +61,6 @@ def classfmail():
         else:
             suggestion = "Please make sure your input is a plain text"
             return render_template("apology.html", suggestion=suggestion)
-
 
 
 if __name__ == '__main__':
